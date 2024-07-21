1a5a6618cdce564c9a5e85453415d8ddbd1b415c
nimna-thiranjaya <nimnathiranjaya523@gmail.com>
2024-07-21 19:02:31 +0530

    db script changes and execution steps

--- dbscripts/masterDataDBScript.js
+++ dbscripts/masterDataDBScript.js
@@ -1,3 +1,6 @@
+// Define the current timestamp
+const currentTimestamp = new Date();
+
 // Drop collections if they already exist to ensure clean insertion
 db.genders.drop();
 db.identityTypes.drop();
@@ -43,6 +46,9 @@ db.identityType.insertMany([
     },
 ]);
 
+const year = currentTimestamp.getFullYear();
+const month = currentTimestamp.getMonth() + 1;
+const workingDate = new Date(year, month, 1);
 // Insert master data for company info
 db.companyInfo.insertMany([
     {
@@ -50,9 +56,9 @@ db.companyInfo.insertMany([
         name: 'Sri Lanka Personal Drivers',
         address: 'Colombo 07',
         image: 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
-        workingYear: new Date().getFullYear(),
-        workingMonth: new Date().getMonth() + 1,
-        workingDate: new Date(Date().getFullYear(), Date().getMonth() + 1, 1),
+        workingYear: year,
+        workingMonth: month,
+        workingDate: workingDate,
         status: 1,
         createdAt: currentTimestamp,
         updatedAt: currentTimestamp,
