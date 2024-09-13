ce5d149fe6df4f017f168b9557fdf9dfb5c69557
nimnathiranjaya523 <nimnathiranjaya523@gmail.com>
2024-09-13 09:18:34 +0000

    Vehical init

---
+++ src/modules/vehicle/vehicle.controller.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from "express";
+
+// Save vehicle
+const saveVehicle = (req : Request, res : Response) => {
+
+}
+
+// Update vehicle
+const updateVehicle = (req : Request, res : Response) => {
+
+}
+
+// Get by id
+const getVehicleById = (req : Request, res : Response) => {
+
+}
+
+// Get all
+const getAllVehicles = (req : Request, res : Response) => {
+
+}
+
+// delete by id
+const deleteVehicleById = (req : Request, res : Response) => {
+
+}
+
+// activeInactiveVehicles
+const activeInactiveVehicle = (req : Request, res : Response) => {
+
+}
+
+export {
+    saveVehicle,
+    updateVehicle,
+    getVehicleById,
+    getAllVehicles,
+    deleteVehicleById,
+    activeInactiveVehicle
+}
