79583f33bf1bb31b239d30ab2df989b11282fbfb
nimna-thiranjaya <nimnathiranjaya523@gmail.com>
2024-12-22 11:02:16 +0530

    change month audit model

--- src/modules/monthAudit/monthAudit.model.ts
+++ src/modules/monthAudit/monthAudit.model.ts
@@ -1,27 +1,33 @@
 import mongoose from 'mongoose';
 import { WellKnownStatus } from '../../util/enums/well-known-status.enum';
 
-const monthAuditSchema = new mongoose.Schema({
-    newWorkingDate: {
-        type: Date,
-        required: true,
-    },
+const monthAuditSchema = new mongoose.Schema(
+    {
+        newWorkingDate: {
+            type: Date,
+            required: true,
+        },
 
-    status: {
-        type: Number,
-        required: true,
-        default: WellKnownStatus.ACTIVE,
-    },
+        status: {
+            type: Number,
+            required: true,
+            default: WellKnownStatus.ACTIVE,
+        },
 
-    createdBy: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-    },
+        createdBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+        },
 
-    updatedBy: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
+        updatedBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+        },
     },
-});
+    {
+        timestamps: true,
+        versionKey: false,
+    }
+);
 
 export default mongoose.model('MonthAudit', monthAuditSchema);
